b939bfb299aaaaf87487348ab3a093dc07d97df0
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-12-21 11:33:49 +0700

    done carousel styling

--- src/pages/Portfolio/index.tsx
+++ src/pages/Portfolio/index.tsx
@@ -3,7 +3,12 @@ import CardPortfolio from "../../components/Card/Portfolio";
 import Porto1 from "../../assets/images/porto-1.png";
 import Porto2 from "../../assets/images/porto-2.png";
 import Porto3 from "../../assets/images/porto-3.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useRef, useState } from "react";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Portfolio = () => {
   const refCarouselPorto = useRef<null | HTMLDivElement>(null);
@@ -56,55 +61,55 @@ const Portfolio = () => {
   return (
     <Container>
       <h1 className="text-[22px] font-semibold">Portfolios</h1>
-      <div className="flex">
+      <div className="relative">
         <button
-          className="bg-gray-200 px-3 py-2 hover:bg-gray-300"
+          className="absolute bottom-1/2 left-0 h-14 w-14 rounded-full bg-white shadow-xl hover:bg-slate-100"
           onClick={prev}
         >
-          Prev
+          <FontAwesomeIcon icon={faChevronLeft} className="text-secondary" />
         </button>
         <button
-          className="ml-2 bg-gray-200 px-3 py-2 hover:bg-gray-300"
+          className="absolute bottom-1/2 right-0 h-14 w-14 rounded-full bg-white shadow-xl hover:bg-slate-100"
           onClick={next}
         >
-          Next
+          <FontAwesomeIcon icon={faChevronRight} className="text-secondary" />
         </button>
-      </div>
-      <div
-        className="flex overflow-x-auto"
-        id="carousel-porto"
-        ref={refCarouselPorto}
-      >
-        <CardPortfolio
-          image={Porto1}
-          title={"Porto 1"}
-          description={"Description"}
-          className={"carousel-item"}
-        />
-        <CardPortfolio
-          image={Porto2}
-          title={"Porto 2"}
-          description={"Description"}
-          className={"carousel-item"}
-        />
-        <CardPortfolio
-          image={Porto3}
-          title={"Porto 3"}
-          description={"Description"}
-          className={"carousel-item"}
-        />
-        <CardPortfolio
-          image={Porto2}
-          title={"Porto 4"}
-          description={"Description"}
-          className={"carousel-item"}
-        />
-        <CardPortfolio
-          image={Porto3}
-          title={"Porto 5"}
-          description={"Description"}
-          className={"carousel-item"}
-        />
+        <div
+          className="flex overflow-x-auto whitespace-nowrap scrollbar-hide"
+          id="carousel-porto"
+          ref={refCarouselPorto}
+        >
+          <CardPortfolio
+            image={Porto1}
+            title={"Porto 1"}
+            description={"Description"}
+            className={"carousel-item"}
+          />
+          <CardPortfolio
+            image={Porto2}
+            title={"Porto 2"}
+            description={"Description"}
+            className={"carousel-item"}
+          />
+          <CardPortfolio
+            image={Porto3}
+            title={"Porto 3"}
+            description={"Description"}
+            className={"carousel-item"}
+          />
+          <CardPortfolio
+            image={Porto2}
+            title={"Porto 4"}
+            description={"Description"}
+            className={"carousel-item"}
+          />
+          <CardPortfolio
+            image={Porto3}
+            title={"Porto 5"}
+            description={"Description"}
+            className={"carousel-item"}
+          />
+        </div>
       </div>
     </Container>
   );
