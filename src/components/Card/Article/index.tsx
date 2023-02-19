7141779fa9f7c89a7e0a918a628743352755222b
Abu Abdirohman <abuabdirohman4@gmail.com>
2023-02-19 20:50:25 +0700

    feat: add list article

--- src/components/Card/Article/index.tsx
+++ src/components/Card/Article/index.tsx
@@ -1,30 +1,37 @@
 type Props = {
   image?: string;
   title: string;
-  count: number;
+  desc: string;
+  link: string;
 };
 
-const CardArticle = ({ image, title = "Title", count = 1 }: Props) => {
+const CardArticle = ({
+  image,
+  title = "Title",
+  desc = "",
+  link = "",
+}: Props) => {
   return (
-    <div className="my-1 w-full px-1 md:w-1/2 lg:my-4 lg:w-1/4 lg:px-4">
-      <div className="overflow-hidden rounded-xl border border-gray-200">
-        <a href="">
+    <div className="my-1 w-full px-1 transition-all hover:scale-105 md:w-1/2 lg:my-4 lg:w-1/4 lg:px-4">
+      <div className="h-64 overflow-hidden rounded-xl border border-gray-200">
+        <a href={link} target="_blank">
           <div className="m-4 overflow-hidden rounded-xl">
-            <img alt="title" className="block h-auto w-full" src={image} />
+            <img alt="title" className="block h-32 w-full" src={image} />
           </div>
         </a>
 
         <header className="mb-4 px-4 leading-tight">
           <h1 className="text-lg">
             <a
-              className="font-semibold text-black no-underline hover:underline"
-              href=""
+              className="font-semibold text-black no-underline line-clamp-3 hover:underline"
+              href={link}
+              target="_blank"
             >
               {title}
             </a>
           </h1>
           <span className="block text-sm font-light text-gray-500 no-underline">
-            {count} items
+            {desc}
           </span>
         </header>
       </div>
