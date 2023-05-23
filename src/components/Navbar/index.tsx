2f0103520aca713758178988b980765c415e38a7
abuabdirohman4 <abuabdirohman4@gmail.com>
2023-05-23 17:23:06 +0700

    add confition close menu after click menu in tablet

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -7,7 +7,8 @@ const Navbar = () => {
   const className =
     "align-center flex border-b border-gray-100 py-2 pl-3 pr-4 text-base font-normal text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-indigo-600 md:dark:hover:bg-transparent md:dark:hover:text-white";
   const clickOnMobile = () => {
-    window.innerWidth < 768 && setMenuActive((prev) => !prev);
+    // Mobile & Tablet
+    window.innerWidth < 1024 && setMenuActive((prev) => !prev);
   };
 
   return (
